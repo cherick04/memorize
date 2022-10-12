494d0c32c6efd6d3c0b35714560f7a5a236d653e
Erick Chacon <cherickdev@gmail.com>
2022-10-12 11:10:45 -0400

    Show more details of all themes in `ThemeManager`

--- Memorize/View/ThemeManager.swift
+++ Memorize/View/ThemeManager.swift
@@ -10,12 +10,31 @@ import SwiftUI
 struct ThemeManager: View {
     @EnvironmentObject var store: ThemeStore
     
+    @Environment(\.presentationMode) private var presentationMode
+    
     var body: some View {
         NavigationView {
             List {
                 ForEach(store.themes) { theme in
                     VStack(alignment: .leading) {
-                        Text(theme.name)
+                        HStack {
+                            colorSample(for: theme)
+                            Text(theme.name)
+                            Spacer()
+                            Text("Card Count: \(theme.emojis.count)")
+                        }
+                        emojiSamples(for: theme)
+                    }
+                }
+            }
+            .navigationTitle("Themes")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    if presentationMode.wrappedValue.isPresented,
+                        UIDevice.current.userInterfaceIdiom != .pad {
+                        Button("Close") {
+                            presentationMode.wrappedValue.dismiss()
+                        }
                     }
                 }
             }
@@ -25,6 +44,14 @@ struct ThemeManager: View {
     private func colorSample(for theme: Theme) -> some View {
         Rectangle()
             .foregroundColor(Color.byRGBA(theme.color))
-            .frame(width: 25, height: 25)
+            .frame(width: 15, height: 15)
+    }
+    
+    private func emojiSamples(for theme: Theme) -> some View {
+        HStack {
+            ForEach(theme.emojis, id: \.self) { emoji in
+                Text(emoji)
+            }
+        }
     }
 }
