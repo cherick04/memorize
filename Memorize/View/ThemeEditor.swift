682631a19d7e0fc79623c98a937cc888eebd145b
Erick Chacon <cherickdev@gmail.com>
2022-10-13 08:37:21 -0400

    Add sections in `ThemeEditor` for number of card pairs, color, adding and removing emojis

--- Memorize/View/ThemeEditor.swift
+++ Memorize/View/ThemeEditor.swift
@@ -10,16 +10,89 @@ import SwiftUI
 struct ThemeEditor: View {
     @Binding var theme: Theme
     
+    @State private var cardCount = Constants.cardCountMin
+    @State private var color = Color.white
+    @State private var emojisToAdd = ""
+    
     var body: some View {
         Form {
             nameSection
+            cardPairSection
+            colorSection
+            addEmojiSection
+            removeEmojiSection
         }
+        .navigationTitle("Edit \(theme.name)")
     }
     
     private var nameSection: some View {
-        Section("Name") {
+        Section("Theme Name") {
             TextField("Name", text: $theme.name)
         }
+    }
+    
+    private var cardPairSection: some View {
+        Section("Number of card pairs") {
+            Stepper(
+                String(cardCount),
+                onIncrement: {
+                    cardCount += 1
+                }, onDecrement: {
+                    cardCount -= 1
+                    if cardCount < Constants.cardCountMin {
+                        cardCount = Constants.cardCountMin
+                    }
+                }
+            )
+        }
+    }
+    
+    private var colorSection: some View {
+        Section("Color") {
+            ColorPicker("Choose color", selection: $color)
+        }
+    }
+
+    private var addEmojiSection: some View {
+        Section("Add Emojis") {
+            TextField("", text: $emojisToAdd)
+                .onChange(of: emojisToAdd) { emojis in
+                    addEmojis(emojis)
+                }
+        }
+    }
+    
+    private func addEmojis(_ emojis: String) {
+        withAnimation {
+            let newEmojis = emojis
+                .filter { $0.isEmoji }
+                .map { String($0) }
+                .removingDuplicateCharacters
+            theme.emojis.append(contentsOf: newEmojis)
+                
+        }
+    }
+    
+    private var removeEmojiSection: some View {
+        Section(header: Text("Remove Emoji")) {
+            let emojis = theme.emojis.removingDuplicateCharacters
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: Constants.emojiSize))]) {
+                ForEach(emojis, id: \.self) { emoji in
+                    Text(emoji)
+                        .onTapGesture {
+                            withAnimation {
+                                theme.emojis.removeAll(where: { $0 == emoji })
+                            }
+                        }
+                }
+            }
+            .font(.system(size: Constants.emojiSize))
+        }
+    }
+    
+    private struct Constants {
+        static let cardCountMin = 2
+        static let emojiSize: CGFloat = 40
     }
 }
 
