31f52a0ba2b293c180dd9029dea7439d5e595e09
Erick Chacon <cherickdev@gmail.com>
2022-10-13 17:42:29 -0400

    Moved `CardView` to its own file

--- Memorize/View/EmojiMemoryGameView.swift
+++ Memorize/View/EmojiMemoryGameView.swift
@@ -65,39 +65,6 @@ struct EmojiMemoryGameView: View {
     }
 }
 
-/// Card View to be reused in game
-struct CardView: View {
-    let card: EmojiMemoryGame.Card
-    
-    var body: some View {
-        GeometryReader { geometry in
-            ZStack {
-                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-                if card.isFaceUp {
-                    shape.fill(.white)
-                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-                    Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90))
-                        .padding(5).opacity(0.5)
-                    Text(card.content).font(fontSize(width: geometry.size.width))
-                } else {
-                    shape.fill()
-                }
-            }
-        }
-    }
-    
-    private func fontSize(width: CGFloat) -> Font {
-        Font.system(size: width * DrawingConstants.fontScale)
-    }
-    
-    private struct DrawingConstants {
-        static let cornerRadius: CGFloat = 15
-        static let lineWidth: CGFloat = 3
-        static let fontScale: CGFloat = 0.7
-    }
-}
-
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let game = EmojiMemoryGame()
